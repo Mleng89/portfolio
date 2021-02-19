53451cb43419b57662e5467f7c49e41821e57e67
Matthew Leng <mleng89@gmail.com>
2021-02-18 22:11:39 -0500

    Prettier: Formatted with rules

--- pages/index.js
+++ pages/index.js
@@ -5,133 +5,133 @@ import Layout from './layout';
 import Typical from 'react-typical';
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main>
-				<Layout title='Home | Matthew Leng'>
-					<div className='container'>
-						<Image src='/pls.png' width='100px' height='100px' />
-						<h2 className='code'>Hello! My name is Matthew Leng</h2>
-						<Typical
-							steps={['I am a software engineer!', 100]}
-							loop='2'
-							wrapper='h3'
-						/>
-					</div>
-				</Layout>
-			</main>
+      <main>
+        <Layout title="Home | Matthew Leng">
+          <div className="container">
+            <Image src="/pls.png" width="100px" height="100px" />
+            <h2 className="code">Hello! My name is Matthew Leng</h2>
+            <Typical
+              steps={['I am a software engineer!', 100]}
+              loop="2"
+              wrapper="h3"
+            />
+          </div>
+        </Layout>
+      </main>
 
-			<footer>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by{' '}
-					<img src='/vercel.svg' alt='Vercel Logo' className='logo' />
-				</a>
-			</footer>
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
 
-			<style jsx>{`
-				.container {
-					min-height: 100vh;
-					min-width: 95vw;
-					padding: 0 0.5rem;
-					display: flex;
-					flex-direction: column;
-					justify-content: center;
-					align-items: center;
-				}
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          min-width: 95vw;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-				main {
-					flex: 1;
-					display: flex;
-					flex-direction: column;
-					justify-content: center;
-					align-items: center;
-				}
+        main {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-				footer {
-					width: 100%;
-					height: 100px;
-					border-top: 1px solid #eaeaea;
-					display: flex;
-					justify-content: center;
-					align-items: center;
-				}
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-				footer img {
-					margin-left: 0.5rem;
-				}
+        footer img {
+          margin-left: 0.5rem;
+        }
 
-				footer a {
-					display: flex;
-					justify-content: center;
-					align-items: center;
-				}
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-				a {
-					color: inherit;
-					text-decoration: none;
-				}
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
 
-				.title a {
-					color: #0070f3;
-					text-decoration: none;
-				}
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
 
-				.title a:hover,
-				.title a:focus,
-				.title a:active {
-					text-decoration: underline;
-				}
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
 
-				.title {
-					margin: 0;
-					line-height: 1.15;
-					font-size: 4rem;
-				}
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
 
-				.title,
-				.description {
-					text-align: center;
-				}
+        .title,
+        .description {
+          text-align: center;
+        }
 
-				.description {
-					line-height: 1.5;
-					font-size: 1.5rem;
-				}
-				code {
-					background: #fafafa;
-					border-radius: 5px;
-					padding: 0.75rem;
-					font-size: 1.1rem;
-					font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-						DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-				}
-				.logo {
-					height: 1em;
-				}
-			`}</style>
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+        .logo {
+          height: 1em;
+        }
+      `}</style>
 
-			<style jsx global>{`
-				html,
-				body {
-					padding: 0;
-					margin: 0;
-					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-						Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-						sans-serif;
-				}
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
 
-				* {
-					box-sizing: border-box;
-				}
-			`}</style>
-		</div>
-	);
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
 }
--- pages/layout.js
+++ pages/layout.js
@@ -3,97 +3,97 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Layout({ children, title = 'Matthew Leng' }) {
-	return (
-		<div className='container'>
-			<Head>
-				<title>{title}</title>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			</Head>
-			<header>
-				<nav>
-					<ul>
-						<li>
-							<Link href='/'>
-								<a>Home</a>
-							</Link>{' '}
-							|
-						</li>
+  return (
+    <div className="container">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>{' '}
+              |
+            </li>
 
-						<li>
-							<Link href='/about'>
-								<a>About</a>
-							</Link>{' '}
-							|
-						</li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>{' '}
+              |
+            </li>
 
-						<li>
-							<Link href='/projects'>
-								<a>Projects</a>
-							</Link>
-						</li>
-					</ul>
-				</nav>
-			</header>
+            <li>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-			{children}
+      {children}
 
-			<footer>
-				<div className='footer_text'>
-					<div>
-						<Link href='https://github.com/Mleng89'>
-							<a>
-								<Image src='/github.svg' width='20px' height='20px' />
-							</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='https://www.linkedin.com/in/matthew-leng/'>
-							<a>
-								<Image src='/linkedin.svg' width='20px' height='20px' />
-							</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='https://twitter.com/matthewleng'>
-							<a>
-								<Image src='/twitter.svg' width='20px' height='20px' />
-							</a>
-						</Link>
-					</div>
-				</div>
-				<p className='footer_name'>© 2021 Matthew Leng</p>
-			</footer>
-			<style jsx>
-				{`
-					.container {
-						border: 1px black solid;
-					}
-					nav {
-						border: 1px black solid;
-						min-height: 55px;
-						min-width: 100px;
-					}
-					ul {
-						display: flex;
-						justify-content: center;
-						list-style-type: none;
-						margin: 0;
-						margin-top: 10px;
-						padding: 0;
-					}
-					.footer_text {
-						display: flex;
-						justify-content: center;
-					}
-					.footer_text > div {
-						margin-left: 10px;
-					}
-					.footer_name {
-						text-align: center;
-					}
-				`}
-			</style>
-		</div>
-	);
+      <footer>
+        <div className="footer_text">
+          <div>
+            <Link href="https://github.com/Mleng89">
+              <a>
+                <Image src="/github.svg" width="20px" height="20px" />
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.linkedin.com/in/matthew-leng/">
+              <a>
+                <Image src="/linkedin.svg" width="20px" height="20px" />
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://twitter.com/matthewleng">
+              <a>
+                <Image src="/twitter.svg" width="20px" height="20px" />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <p className="footer_name">© 2021 Matthew Leng</p>
+      </footer>
+      <style jsx>
+        {`
+          .container {
+            border: 1px black solid;
+          }
+          nav {
+            border: 1px black solid;
+            min-height: 55px;
+            min-width: 100px;
+          }
+          ul {
+            display: flex;
+            justify-content: center;
+            list-style-type: none;
+            margin: 0;
+            margin-top: 10px;
+            padding: 0;
+          }
+          .footer_text {
+            display: flex;
+            justify-content: center;
+          }
+          .footer_text > div {
+            margin-left: 10px;
+          }
+          .footer_name {
+            text-align: center;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
